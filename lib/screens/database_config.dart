036001cfed5b8a0b5023e90ad870cb9edca29bde
gagjunior <gagjunior@hotmail.com>
2023-03-20 23:39:09 -0300

    tela de configuração da base de dados

--- lib/screens/database_config.dart
+++ lib/screens/database_config.dart
@@ -8,38 +8,108 @@ class DataBaseConfig extends StatefulWidget {
 }
 
 class _DataBaseConfigState extends State<DataBaseConfig> {
+  final FontWeight _titleLabel = FontWeight.w600;
+
   @override
   Widget build(BuildContext context) {
     return ScaffoldPage(
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(height: 100),
-          Center(
-              child: Text(
-            'Configuração da Base de Dados',
-            softWrap: true,
-            style: TextStyle(
-              fontSize: 26,
-              fontWeight: FontWeight.w600,
-              color: Colors.blue.dark,
-            ),
-          )),
-          const SizedBox(height: 60),
-          Center(
-            child: InfoLabel(
-                label: 'Url de conexão com o banco de dados',
-                labelStyle: TextStyle(
-                  fontSize: 16,
-                ),
-                child: const SizedBox(
-                  width: 500,
-                  child: TextBox(
-                    placeholder: 'Exemplo: mongodb://localhost:27017',
+      content: Center(
+        heightFactor: 1.5,
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //const SizedBox(height: 20),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Configuração da Base de Dados',
+                  softWrap: true,
+                  style: TextStyle(
+                    color: Colors.blue.dark,
+                    fontSize: 24,
                   ),
-                )),
-          )
-        ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Flexible(
+              flex: 2,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 500),
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: InfoLabel(
+                    label: 'Banco de Dados',
+                    labelStyle: TextStyle(fontWeight: _titleLabel),
+                    child: SelectableText.rich(
+                      TextSpan(
+                          text: 'O aplicativo ',
+                          style: DefaultTextStyle.of(context).style,
+                          children: [
+                            TextSpan(
+                                text: 'ArchBoxControl ',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue.dark)),
+                            TextSpan(
+                                text: 'utiliza como base de dados o ',
+                                style: DefaultTextStyle.of(context).style),
+                            TextSpan(
+                                text: 'MongoDb.',
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            TextSpan(
+                                text:
+                                    '\n\nVocê pode configurar um banco de dados ',
+                                style: DefaultTextStyle.of(context).style),
+                            TextSpan(
+                                text: 'Local ',
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            TextSpan(
+                                text: 'ou em nuvem com o ',
+                                style: DefaultTextStyle.of(context).style),
+                            TextSpan(
+                                text: 'MongoDB Atlas. ',
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            TextSpan(
+                                text:
+                                    '\n\nPara isso, é só informar a URL de conexão no campo abaixo.',
+                                style: DefaultTextStyle.of(context).style),
+                          ]),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Flexible(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 500),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InfoLabel(
+                    label: 'URL de Conexão com a Base de Dados',
+                    labelStyle: TextStyle(fontWeight: _titleLabel),
+                    child:
+                        const TextBox(placeholder: 'mongodb://localhost:27017'),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Flexible(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 500),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SelectableText(
+                      'Caso seja necessário configurar o aplicativo para conectar com o MongoDB hospedado em outro serviço de nuvem, favor contactar o suporte'),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

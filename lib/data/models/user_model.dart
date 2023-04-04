17019ac281dd370142d49bc3486dbaaa97cf775a
gagjunior <gagjunior@hotmail.com>
2023-04-03 23:38:35 -0300

    users

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -2,40 +2,40 @@ class UserModel {
   String _name;
   String _password;
   final String _email;
-  String? _department;
-  String? _profile;
+  String? department;
+  String? profile;
 
-  UserModel(this._name, this._email, this._password);
+  UserModel(this._name, this._email, this._password,
+      {this.department, this.profile});
 
-  String get getName => _name;
+  String get name => _name;
 
-  set setName(String name) {
+  set name(String name) {
     if (name.isNotEmpty && name != '' && name != ' ') {
       _name = name;
     }
   }
 
-  String? get getDepartment => _department;
-  set setDepartment(String department) => _department = department;
-
-  String? get getProfile => _profile;
-  set setProfile(String profile) => _profile = profile;
-
-  String get getEmail => _email;
-
-  set setPassword(String password) {
-    if (password.isNotEmpty && password != '' && password != ' ') {
-      _password = password;
-    }
-  }
+  String get email => _email;
+  set password(String value) => _password = value;
 
   Map<String, dynamic> get toMap {
     return {
       'name': _name,
       'email': _email,
-      'senha': _password,
-      'department': _department,
-      'profile': _profile,
+      'password': _password,
+      'department': department,
+      'profile': profile,
     };
+  }
+
+  UserModel toUser(Map<String, dynamic> userData) {
+    String name = userData['name'];
+    String email = userData['email'];
+    String password = userData['password'];
+    String department = userData['department'];
+    String profile = userData['profile'];
+    return UserModel(name, email, password,
+        department: department, profile: profile);
   }
 }

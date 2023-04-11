9fd3d7453635c1d2eec96e0000816ef7353a5c25
gagjunior <gagjunior@hotmail.com>
2023-04-10 23:39:14 -0300

    create user exceptions

---
+++ lib/exceptions/user_adm_exception.dart
@@ -0,0 +1,29 @@
+class NameUserException implements Exception {
+  final String _msg;
+  NameUserException(this._msg);
+
+  @override
+  String toString() {
+    return _msg;
+  }
+}
+
+class EmailUserException implements Exception {
+  final String _msg;
+  EmailUserException(this._msg);
+
+  @override
+  String toString() {
+    return _msg;
+  }
+}
+
+class PasswordUserException implements Exception {
+  final String _msg;
+  PasswordUserException(this._msg);
+
+  @override
+  String toString() {
+    return _msg;
+  }
+}

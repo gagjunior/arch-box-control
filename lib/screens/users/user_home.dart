73af1cf447b363a3a74a6b9e375546d81c2a1c74
gagjunior <gagjunior@hotmail.com>
2023-05-04 09:22:39 -0300

    user list screen

--- lib/screens/users/user_home.dart
+++ lib/screens/users/user_home.dart
@@ -1,16 +1,40 @@
+import 'package:arch_box_control/screens/users/user_list.dart';
 import 'package:easy_localization/easy_localization.dart' as easy;
-import 'package:fluent_ui/fluent_ui.dart' ;
+import 'package:fluent_ui/fluent_ui.dart';
 
 class UserHomeScreen extends StatelessWidget {
   const UserHomeScreen({Key? key}) : super(key: key);
+  final SizedBox _vSpacer = const SizedBox(
+    height: 10,
+  );
 
   @override
   Widget build(BuildContext context) {
     return ScaffoldPage.scrollable(
       header: PageHeader(
-        title: Text(easy.plural('user', 3), style: TextStyle(color: Colors.blue.dark)),
+        title: Text(easy.plural('user', 3),
+            style: TextStyle(color: Colors.blue.dark)),
       ),
-      children: [],
+      children: [
+        Button(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(easy.tr('list_users'), textAlign: TextAlign.left),
+          ),
+          onPressed: () {
+            Navigator.push(context,
+                FluentPageRoute(builder: (context) => UserListScreen()));
+          },
+        ),
+        _vSpacer,
+        Button(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(easy.tr('register_user'), textAlign: TextAlign.left),
+          ),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
---
+++ lib/screens/users/user_list.dart
@@ -0,0 +1,10 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class UserListScreen extends StatelessWidget {
+  const UserListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

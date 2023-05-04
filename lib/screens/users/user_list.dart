9508f9b5943d7277b7182b2139c17a21b2b9f786
gilberto.junior <gilberto.junior@cbbasfaltos.com.br>
2023-05-04 16:52:42 -0300

    user list screen

--- lib/screens/users/user_list.dart
+++ lib/screens/users/user_list.dart
@@ -1,10 +1,31 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:easy_localization/easy_localization.dart' as easy;
 
 class UserListScreen extends StatelessWidget {
-  const UserListScreen({super.key});
+  UserListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScaffoldPage.scrollable(
+      header: PageHeader(
+        title: Text(
+          easy.tr('user_list'),
+        ),
+      ),
+      children: [
+        Card(
+          child: CommandBar(
+            overflowBehavior: CommandBarOverflowBehavior.wrap,
+            compactBreakpointWidth: 600,
+            primaryItems: commandItens,
+          ),
+        ),
+      ],
+    );
   }
+
+  final List<CommandBarItem> commandItens = [
+    CommandBarButton(
+        icon: Icon(FluentIcons.list), onPressed: () {}, label: Text('Listar'))
+  ];
 }

aa488b38d8bdd8b4c63848b6e2356015a1f6fda4
gagjunior <gagjunior@hotmail.com>
2023-05-12 23:48:11 -0300

    list actions for users

--- lib/screens/users/user_home.dart
+++ lib/screens/users/user_home.dart
@@ -1,47 +1,41 @@
-import 'package:arch_box_control/screens/users/user_list.dart';
 import 'package:easy_localization/easy_localization.dart' as easy;
 import 'package:fluent_ui/fluent_ui.dart';
 
 class UserHomeScreen extends StatelessWidget {
   const UserHomeScreen({Key? key}) : super(key: key);
-  final SizedBox _vSpacer = const SizedBox(height: 10);
 
   @override
   Widget build(BuildContext context) {
-    return ScaffoldPage.scrollable(
+    const actions = [
+      'Adicionar',
+      'Listar',
+    ];
+    const icons = [FluentIcons.add, FluentIcons.list];
+    const subtitles = ['Adiconar novos usuários', 'Listar todos os usuários'];
+
+    return ScaffoldPage(
       header: PageHeader(
         title: Text(easy.plural('user', 3),
             style: TextStyle(color: Colors.blue.dark)),
       ),
-      children: [
-        Button(
-          style: ButtonStyle(
-            elevation: ButtonState.all(2),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(14),
-            child: Text(
-              easy.tr('list_users'),
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+      content: ListView.builder(
+        itemCount: actions.length,
+        itemBuilder: (context, index) {
+          final action = actions[index];
+          return Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: ListTile.selectable(
+              selectionMode: ListTileSelectionMode.single,
+              onPressed: () {},
+              leading: CircleAvatar(
+                child: Icon(icons[index]),
               ),
+              title: Text(action),
+              subtitle: Text(subtitles[index]),
             ),
-          ),
-          onPressed: () {
-            Navigator.push(context,
-                FluentPageRoute(builder: (context) => UserListScreen()));
-          },
-        ),
-        _vSpacer,
-        Button(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(easy.tr('register_user'), textAlign: TextAlign.left),
-          ),
-          onPressed: () {},
-        ),
-      ],
+          );
+        },
+      ),
     );
   }
 }
